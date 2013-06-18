857bed598587cf5189d85d1ba29d92c9c7ebf0d5
Jedisto <jedisto@gmail.com>
2013-06-18 22:00:01 +0800

    activiti升级到5.12.1

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,14 @@
 # Load the rails application
 
+require 'java'
+$CLASSPATH << File.join(Rails.root, "config")
+
 require 'deps/activiti-engine-5.12.1.jar'
 require 'deps/activiti-bpmn-model-5.12.1.jar'
 require 'deps/spring-core-3.1.2.RELEASE.jar'
 require 'deps/spring-beans-3.1.2.RELEASE.jar'
 require 'deps/spring-asm-3.1.2.RELEASE.jar'
+require 'deps/slf4j-log4j12-1.7.5.jar'
 require 'deps/slf4j-api-1.7.2.jar'
 require 'deps/log4j-1.2.17.jar'
 require 'deps/commons-logging-1.1.1.jar'
@@ -12,8 +16,7 @@ require 'deps/mybatis-3.1.1.jar'
 require 'deps/postgresql-9.1-901.jdbc4.jar'
 require 'deps/mysql-connector-java-5.1.25-bin.jar'
 
-activiti_process_engine = org.activiti.engine.ProcessEngineConfiguration.createProcessEngineConfigurationFromResource('activiti.cfg.xml').buildProcessEngine
-org.activiti.engine.ProcessEngines.registerProcessEngine activiti_process_engine
+activiti_process_engine = org.activiti.engine.ProcessEngines.getDefaultProcessEngine
 
 Activiti = {
   runtime: activiti_process_engine.getRuntimeService,
