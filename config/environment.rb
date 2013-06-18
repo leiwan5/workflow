39df6ef69668370ace5992b73203ff673c9f0d94
Daniel King <danielking@localhost.(none)>
2013-06-18 21:03:07 +0800

    增加mysql配置

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,7 @@ require 'deps/spring-asm-3.1.2.RELEASE.jar'
 require 'deps/commons-logging-1.1.1.jar'
 require 'deps/mybatis-3.1.1.jar'
 require 'deps/postgresql-9.1-901.jdbc4.jar'
+require 'deps/mysql-connector-java-5.1.25-bin.jar'
 
 import org.activiti.engine.ProcessEngines
 
